1e9725a5a60476890fd7c2d9a0f0d5c4cfece973
Milo Knowles <miloknowles97@gmail.com>
2023-11-19 08:17:57 -0500

    Emoji

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🌲</text></svg>"
+        />
+      </head>
       <body className={Circular.variable}>
         <Theme appearance="dark" accentColor="indigo" radius="medium">
           <Navigation/>
